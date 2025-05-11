9a4003cd29aa60a41aaea920ea23ef62c449d25c
Driver-C <driver_c@foxmail.com>
2025-05-11 08:59:54 +0800

    Update evolve version to v1.4.4

--- src/seasons.js
+++ src/seasons.js
@@ -234,6 +234,8 @@ export function seasonDesc(type){
             return astrologyDescription();
         case 'astrology':
             return astrologySymbol();
+        case 'season':
+            return seasonDescription();
     }
 }
 
@@ -304,7 +306,7 @@ export function astroVal(sign){
         case 'aries': // Combat Rating
             return [boosted ? Math.round(12 * multiplier) : Math.round(10 * multiplier)];
         case 'taurus': // Unification Bonus
-            return [boosted ? 3 * multiplier : 2 * multiplier];
+            return [+(boosted ? 3 * multiplier : 2 * multiplier).toFixed(2)];
         case 'gemini': // Knowledge
             return [boosted ? Math.round(30 * multiplier) : Math.round(20 * multiplier)];
         case 'cancer': // Soldier Healing
@@ -392,4 +394,17 @@ function astroEffect(sign){
 function astrologySymbol(){
     let sign = astrologySign();
     return loc(`sign_${sign}_symbol`);
+}
+
+function seasonDescription() {
+    switch (global.city.calendar.season) {
+        case 0:
+            return loc('season_spring');
+        case 1:
+            return loc('season_summer');
+        case 2:
+            return loc('season_autumn');
+        case 3:
+            return loc('season_winter');
+    }
 }
--- src/vars.js
+++ src/vars.js
@@ -66,12 +66,14 @@ Math.rand = function(min, max) {
 
 global['seed'] = 2;
 global['warseed'] = 2;
-export function seededRandom(min, max, alt) {
+export function seededRandom(min, max, alt, useSeed) {
     max = max || 1;
     min = min || 0;
 
-    global[alt ? 'warseed' : 'seed'] = (global[alt ? 'warseed' : 'seed'] * 9301 + 49297) % 233280;
-    let rnd = global[alt ? 'warseed' : 'seed'] / 233280;
+    let seed = useSeed || global[alt ? 'warseed' : 'seed'];
+    let newSeed = (seed * 9301 + 49297) % 233280;
+    let rnd = newSeed / 233280;
+    if (!useSeed){ global[alt ? 'warseed' : 'seed'] = newSeed; }
     return min + rnd * (max - min);
 }
 
@@ -1227,8 +1229,28 @@ if (convertVersion(global['version']) < 104001){
     }
 }
 
-global['version'] = '1.4.1';
-global['revision'] = 'a';
+if (convertVersion(global['version']) < 104002){
+    if(global.city['amphitheatre'] && !global.city.amphitheatre.hasOwnProperty('evil')){
+        global.city.amphitheatre['evil'] = 0;
+    }
+}
+
+if (convertVersion(global['version']) < 104003){
+    if (global.portal.hasOwnProperty('observe') && global.portal.observe.hasOwnProperty('stats')){
+        global.portal.observe.stats.period.gems['compactor'] ??= 0;
+        global.portal.observe.stats.total.gems['compactor'] ??= 0;
+    }
+}
+
+if (convertVersion(global['version']) <= 104003){
+    if(global.race['pet'] && !global.race.pet.hasOwnProperty('event')){
+        global.race.pet['event'] = 0;
+        global.race.pet['pet'] = 0;
+    }
+}
+
+global['version'] = '1.4.4';
+delete global['revision'];
 delete global['beta'];
 
 if (!global.hasOwnProperty('prestige')){
@@ -1287,6 +1309,15 @@ if (!global.settings.space.hasOwnProperty('home')){
 }
 
 setRegionStates(false);
+
+if(!global.race.hasOwnProperty('inactiveTraits')){
+    if(global.race['forager']){
+        global.race.inactiveTraits = {herbivore:global.race['forager'], carnivore:global.race['forager']};
+    }
+    else{
+        global.race.inactiveTraits = {};
+    }
+}
 
 if (!global.settings['icon']){
     global.settings['icon'] = 'star';
@@ -1560,6 +1591,9 @@ export function setupStats(){
     }
     if (global.stats['death_tour'] && !global.stats.death_tour.hasOwnProperty('md')){
         global.stats.death_tour['md'] = { l: 0, h: 0, a: 0, e: 0, m: 0, mg: 0 };
+    }
+    if (!global.stats['warlord']){
+        global.stats['warlord'] = { k: false, p: false, a: false, r: false, g: false };
     }
 }
 
@@ -1937,14 +1971,13 @@ if (!global.civic['new']){
 }
 
 if (!global.race['purgatory']){
-    global.race['purgatory'] = {
-        city: {},
-        space: {},
-        portal: {},
-        eden: {},
-        tech: {},
-    };
+    global.race['purgatory'] = {};
 }
+['city', 'space', 'portal', 'eden', 'tech'].forEach((item) => {
+    if(!global.race['purgatory'][item]){
+        global.race['purgatory'][item] = {};
+    }
+})
 
 if (!global.civic['d_job']){
     if (global.race['carnivore'] || global.race['soul_eater']){
@@ -2228,7 +2261,7 @@ window.soft_reset = function reset(source){
     window.location.reload();
 }
 
-export var webWorker = { w: false, s: false, mt: 250 };
+export var webWorker = { w: false, s: false, mt: 250, midRatio: 4, longRatio: 20 };
 export var intervals = {};
 
 export function clearSavedMessages(){
@@ -2254,7 +2287,7 @@ function setRegionStates(reset){
             'nebula','neutron','blackhole','sirius','stargate','gateway','gorddon',
             'alien1','alien2','chthonian','titan','enceladus','triton','eris','kuiper'
         ],
-        portal: ['fortress','badlands','pit','ruins','gate','lake','spire'],
+        portal: ['fortress','badlands','pit','ruins','gate','lake','spire','wasteland'],
         eden: ['asphodel','elysium','isle','palace'],
         tau: ['home','red','roid','gas','gas2','star']
     };
